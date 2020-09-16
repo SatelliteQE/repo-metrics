4a5389f73b7d2c1f61aaad9aea7fe6bba60d70db
Mike Shriver <mshriver@redhat.com>
2020-09-16 07:57:13 -0400

    Update command names and options, update readme

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
 Basic tooling for gathering specific metrics about GH prs.
 
+# Configuration and Installation
+1. Clone the repository to your local filesystem, and create a python 3.7+ virtualenv to run in.
+2. Copy `settings.yaml.example` to `settings.yaml` and modify as you need for your metrics collection.
+3. A GitHub API token needs to be generated and set in `settings.yaml` as `gh_token`. It needs read permissions.
+4. Reviewer teams need to be defined to classify tier1, tier2 reviews. This tool assumes you use a two tier review process.
+5. `pip install -e .` in your new virtualenv, which will install the `github-metrics` command.
 
-Command structure:
+
+# Command structure
 
 `github-metrics` is the core command, with sub-commands to gather different types of metrics
 
-`github-metrics gather prs`
+`github-metrics pr-report`
 This command with gather data related to PRs like comment times, average age of PRs.
 Data will be arranged by PR.
 
 
-`github-metrics gather reviewers`
+`github-metrics reviewer-report`
 This command with gather data related to reviewers activity like number of reviews in a time period.
 Data will be arranged by reviewer
 
+`--help` is available for both commands, to see available options and their description.
 
-`github-metrics gather contributors`
-This command with gather data related to contributors activity like number of opened PRs, merged PRs
-Data will be arranged by repository
+# Common command options
+
+`--output-file-prefix`
+Prepends the output files, formatted in html tables, with the given string.  Can be set in settings.yaml
+
+`--repo`
+Defines the repository to scan for metrics.
+Can be provided multiple times, separate reports will be generated for each repository
+
+`--org`
+Defines the GitHub organization where the given repository exists
+
+`--pr-count`
+Defines the number of PRs to include in the scan for reporting. Will collect PRs from the latest by number.
--- scripts/gh_metrics.py
+++ scripts/gh_metrics.py
@@ -1,5 +1,3 @@
-# import json
-# from collections import OrderedDict
 from datetime import datetime
 from pathlib import Path
 
@@ -13,20 +11,20 @@ from utils import metrics_calculators
 
 
 # keys that will be read from settings files (dynaconf parsing) for command input defaults
-SETTINGS_OUTPUT_PREFIX = "pr_metrics_output_file_prefix"
+SETTINGS_OUTPUT_PREFIX = "output_file_prefix"
 SETTINGS_REVIEWER_TEAMS = "reviewer_teams"
 
 
-# parent click group for gather and graph commands
+# parent click group for report and graph commands
 @click.group()
-def gather():
+def report():
     pass
 
 
 # reused options for multiple metrics functions
 output_prefix_option = click.option(
-    "--file-output-prefix",
-    default=settings.get(SETTINGS_OUTPUT_PREFIX, "pr-metrics"),
+    "--output-file-prefix",
+    default=settings.get(SETTINGS_OUTPUT_PREFIX, "metrics-report"),
     help="Will only take file name (with or without extension), but not a full path."
     "Will append the metric name an epoch timestamp to the file name.",
 )
@@ -48,67 +46,71 @@ pr_count_option = click.option(
 )
 
 
-@gather.command(
-    "pr-metrics",
+@report.command(
+    "pr-report",
     help="Gather metrics about individual PRs for a GH repo (SatelliteQE/robottelo)",
 )
 @org_name_option
 @repo_name_option
 @output_prefix_option
 @pr_count_option
-def repo_pr_metrics(org, repo, file_output_prefix, pr_count):
+def repo_pr_metrics(org, repo, output_file_prefix, pr_count):
     for repo_name in repo:
+        click.echo(f"Collecting metrics for {org}/{repo_name} ...")
+
         pr_metrics, stat_metrics = metrics_calculators.single_pr_metrics(
             organization=org, repository=repo_name, pr_count=pr_count
         )
 
-        click.echo("-----------------------------------")
-        click.echo(f"Review Metrics By PR for [{repo_name}]")
-        click.echo("-----------------------------------")
+        header = f"Review Metrics By PR for [{repo_name}]"
+        click.echo(f"\n{'-' * len(header)}")
+        click.echo(header)
+        click.echo("-" * len(header))
         click.echo(
             tabulate(pr_metrics, headers="keys", tablefmt="github", floatfmt=".1f")
         )
 
-        click.echo("-----------------------------------")
-        click.echo(f"Review Metric Statistics for [{repo_name}]")
-        click.echo("-----------------------------------")
+        header = f"Review Metric Statistics for [{repo_name}]"
+        click.echo(f"\n{'-' * len(header)}")
+        click.echo(header)
+        click.echo("-" * len(header))
         click.echo(
             tabulate(stat_metrics, headers="keys", tablefmt="github", floatfmt=".1f")
         )
 
         pr_metrics_filename = METRICS_OUTPUT.joinpath(
-            f"{Path(file_output_prefix).stem}-"
+            f"{Path(output_file_prefix).stem}-"
             f"{org}-"
             f"{repo_name}-"
             "pr_metrics-"
             f"{datetime.now().isoformat(timespec='minutes')}.html"
         )
-        click.echo(f"Writing PR metrics as HTML to {pr_metrics_filename}")
+        click.echo(f"\nWriting PR metrics as HTML to {pr_metrics_filename}")
         file_io.write_to_output(
             pr_metrics_filename,
             tabulate(pr_metrics, headers="keys", tablefmt="html", floatfmt=".1f"),
         )
 
         stat_metrics_filename = METRICS_OUTPUT.joinpath(
-            f"{Path(file_output_prefix).stem}-"
+            f"{Path(output_file_prefix).stem}-"
             f"{org}-"
             f"{repo_name}-"
             "stat_metrics-"
             f"{datetime.now().isoformat(timespec='minutes')}.html"
         )
-        click.echo(f"Writing statistics metrics as HTML to {stat_metrics_filename}")
+        click.echo(f"\nWriting statistics metrics as HTML to {stat_metrics_filename}")
         file_io.write_to_output(
             stat_metrics_filename,
             tabulate(stat_metrics, headers="keys", tablefmt="html", floatfmt=".1f"),
         )
 
 
-@gather.command("reviewer-actions")
+@report.command("reviewer-report")
 @org_name_option
 @repo_name_option
 @output_prefix_option
 @pr_count_option
-def reviewer_actions(org, repo, file_output_prefix, pr_count):
+def reviewer_actions(org, repo, output_file_prefix, pr_count):
     """ Generate metrics for tier reviewer groups, and general contributors
 
     Will collect tier reviewer teams from the github org
@@ -116,42 +118,44 @@ def reviewer_actions(org, repo, file_output_prefix, pr_count):
 
     """
     for repo_name in repo:
+        click.echo(f"Collecting metrics for {org}/{repo_name} ...")
+
         t1_metrics, t2_metrics = metrics_calculators.reviewer_actions(
             organization=org, repository=repo_name, pr_count=pr_count
         )
         header = f"Tier1 Reviewer actions by week for [{repo_name}]"
-        click.echo("-" * len(header))
+        click.echo(f"\n{'-' * len(header)}")
         click.echo(header)
         click.echo("-" * len(header))
         click.echo(tabulate(t1_metrics, headers="keys", tablefmt="github"))
 
         header = f"Tier2 Reviewer actions by week for [{repo_name}]"
-        click.echo("-" * len(header))
+        click.echo(f"\n{'-' * len(header)}")
         click.echo(header)
         click.echo("-" * len(header))
         click.echo(tabulate(t2_metrics, headers="keys", tablefmt="github"))
 
         tier1_metrics_filename = METRICS_OUTPUT.joinpath(
-            f"{Path(file_output_prefix).stem}-"
+            f"{Path(output_file_prefix).stem}-"
             f"{org}-"
             f"{repo_name}-"
             "tier1_reviewers-"
             f"{datetime.now().isoformat(timespec='minutes')}.html"
         )
-        click.echo(f"Writing PR metrics as HTML to {tier1_metrics_filename}")
+        click.echo(f"\nWriting PR metrics as HTML to {tier1_metrics_filename}")
         file_io.write_to_output(
             tier1_metrics_filename,
             tabulate(t1_metrics, headers="keys", tablefmt="html"),
         )
 
         tier2_metrics_filename = METRICS_OUTPUT.joinpath(
-            f"{Path(file_output_prefix).stem}-"
+            f"{Path(output_file_prefix).stem}-"
             f"{org}-"
             f"{repo_name}-"
             "tier2_reviewers-"
             f"{datetime.now().isoformat(timespec='minutes')}.html"
         )
-        click.echo(f"Writing PR metrics as HTML to {tier2_metrics_filename}")
+        click.echo(f"\nWriting PR metrics as HTML to {tier2_metrics_filename}")
         file_io.write_to_output(
             tier2_metrics_filename,
             tabulate(t2_metrics, headers="keys", tablefmt="html"),
